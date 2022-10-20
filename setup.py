a02be98f64415cb37f8c17f45b9e8a13c3c636c2
asmivrosa <116254974+asmivrosa@users.noreply.github.com>
2022-10-20 13:55:59 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name = "jewer.py",
+    name = "avima.py",
     version = "0.1",
-    py_modules = ['jewer'],
-    url = "https://github.com/indraxz/jewer.git",
-    author = "indraxz",
+    py_modules = ['avima'],
+    url = "https://github.com/asmivrosa/asmivrosa.git",
+    author = "asmivrosa",
     install_requires = ["pyngrok"]
 )
